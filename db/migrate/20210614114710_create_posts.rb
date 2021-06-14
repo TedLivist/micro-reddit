51f192c72128e775c83c5ad93ef5125be1a181d9
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-06-14 13:06:09 +0100

    Run migration for Post

--- db/migrate/20210614114710_create_posts.rb
+++ db/migrate/20210614114710_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
       t.string :title
-      t.references :User, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end

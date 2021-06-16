0b0eaed91e96035698379ac8dca042f36d6ea7a8
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-06-16 13:04:10 +0100

    Customise error message for presence of post title

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
 
-  validates :title, presence: true, length: { minimum: 7 }
+  validates :title, presence: { message: "must be present" }, length: { minimum: 7 }
 end
